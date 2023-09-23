dd5796c2afffd4d33f25ce8109c1916f3862fcef
Enmanuel Veras <enmanuel.vargas@toptal.com>
2023-09-23 00:06:37 -0400

    feat: set font and personal data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'Enmanuel Veras Blog',
+  author: 'Enmanuel Veras',
+  headerTitle: 'everas.dev',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -11,12 +11,9 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'enmaveras07@gmail.com',
+  github: 'https://github.com/everas7',
+  linkedin: 'https://www.linkedin.com/in/everas7/',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
