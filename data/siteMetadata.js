62b02cc4aae7608e0a44d22b2d41c2516acc078e
Enmanuel Veras <enmanuel.vargas@toptal.com>
2023-09-23 14:50:37 -0400

    feat: add newsletter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -37,7 +37,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'mailchimp',
   },
   search: {
     provider: 'kbar', // kbar or algolia
